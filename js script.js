1998f3b0dd3c733e9edb11ec6b1647366b01a749
Vyshnavi17-11 <135938623+Vyshnavi17-11@users.noreply.github.com>
2023-07-19 15:53:38 +0530

    Add files via upload

---
+++ js script.js
@@ -0,0 +1,42 @@
+function add(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a+b;
+        document.getElementById("answer1").innerHTML="Addition is "+c;
+
+    }
+    function sub(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a-b;
+        document.getElementById("answer1").innerHTML="Subtraction is "+c;
+
+    }
+    function mul(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a*b;
+        document.getElementById("answer1").innerHTML="Multiplication is"+c;
+
+    }
+    function div(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a/b;
+        document.getElementById("answer1").innerHTML="Division is "+c;
+
+    }
+    function expo(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a**b;
+        document.getElementById("answer1").innerHTML="Power is "+c;
+    }
+    function mod(){
+        let a = parseInt(document.getElementById("value1").value);
+        let b =parseInt(document.getElementById("value2").value);
+        let c =a%b;
+        document.getElementById("answer1").innerHTML="Mod is"+c;
+    }
+
+    
